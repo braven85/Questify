e31be9eb3fc6684d681c20d1f9aff1ee7e966d55
Radek <radekie@wp.pl>
2023-02-19 12:51:34 +0100

    port

--- server/index.js
+++ server/index.js
@@ -65,7 +65,7 @@ app.use((err, _, res, __) => {
 });
 
 const uriDb = process.env.DB_URI;
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const connection = mongoose.connect(uriDb, {
   useNewUrlParser: true,
